4df44ba5cea267b4f8a17abaf994a9b341eca042
Neha Sagare <nehasagare40@gmail.com>
2025-03-02 14:33:06 +0530

    1D_Array.java

---
+++ 1D_Array.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class OneDArray {
+public static void main(String[] args) {
+Scanner sc=new Scanner(System.in);
+int a[]=new int[5];
+System.out.println("Enter numbers for array:");
+for(int i=0;i<5;i++)
+{
+a[i]=sc.nextInt();
+}
+for(int i=0;i<5;i++)
+{
+System.out.println(a[i]);
+}
+}
+
+}
